68abea4a3e45f96d318c24345ef15399c108ddcc
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-13 22:34:47 +0300

    little while the light is with you
    Jesus therefore said to them, "Yet a little while the light is with you. Walk while you have the light, that darkness doesn't overtake you. He who walks in the darkness doesn't know where he is going. (John 12:35)

---
+++ task11/task1104/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task11.task1104;
+
+//Jesus therefore said to them, "Yet a little while the light is with you. Walk while you have the light, 
+//that darkness doesn't overtake you. He who walks in the darkness doesn't know where he is going. (John 12:35)
+
+/* 
+Хороши работнички
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Manager extends Employee {
+
+    }
+
+    public class Chief extends Employee {
+
+    }
+
+    public class Employee {
+
+    }
+
+    public class Secretary extends Employee {
+
+    }
+}
