e871e614f440b7087362b507b5e73232287a4b89
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-13 22:27:23 +0300

    But he said this
    But he said this, signifying by what kind of death he should die. (John 12:33)

---
+++ task11/task1102/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task11.task1102;
+
+//But he said this, signifying by what kind of death he should die. (John 12:33)
+
+/* 
+Домашние животные
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+}
