c123582b311b9cc0544504a0f7482f62326adc0d
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-13 22:58:48 +0300

    become children of light
    While you have the light, believe in the light, that you may become children of light." Jesus said these things, and he departed and hid himself from them. (John 12:36)

---
+++ task11/task1105/Solution.java
@@ -0,0 +1,49 @@
+package com.javarush.task.task11.task1105;
+
+//While you have the light, believe in the light, that you may become children of light."
+//Jesus said these things, and he departed and hid himself from them. (John 12:36)
+
+/* 
+ИТ-компания
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Worker {
+
+    }
+
+    public class Clerk extends Worker {
+
+    }
+
+    public class IT extends Worker {
+
+    }
+
+    public class Programmer extends IT {
+
+    }
+
+    public class ProjectManager extends IT {
+
+    }
+
+    public class CTO extends IT {
+
+    }
+
+    public class OfficeManager extends Clerk {
+
+    }
+
+    public class HR extends Clerk {
+
+    }
+
+    public class Cleaner  extends Clerk {
+
+    }
+}
