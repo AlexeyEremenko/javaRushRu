ba09b2e2245147abc3ce7d8415e0fce30eddbcf5
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-13 22:24:12 +0300

    will draw all men unto me
    And I, if I be lifted up from the earth, will draw all men unto me. (John 12:32)

---
+++ task11/task1101/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task11.task1101;
+
+//And I, if I be lifted up from the earth, will draw all men unto me. (John 12:32)
+
+/* 
+Лошадь и Пегас
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegas extends Horse {
+
+    }
+}
