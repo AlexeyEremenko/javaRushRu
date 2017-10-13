04c20d4cb9bc92af849be331590a2f6f9114dd2d
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-13 22:31:32 +0300

    The Son of Man must be lifted up
    The multitude answered him, "We have heard out of the law that the Christ remains forever. How do you say, 'The Son of Man must be lifted up?' Who is this Son of Man?" (John 12:34)

---
+++ task11/task1103/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task11.task1103;
+
+//The multitude answered him, "We have heard out of the law that the Christ remains forever. 
+//How do you say, 'The Son of Man must be lifted up?' Who is this Son of Man?" (John 12:34)
+
+/* 
+Межпланетная ферма
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Animal {
+
+    }
+
+    public class Cow extends Animal{
+
+    }
+
+    public class Pig extends Animal{
+
+    }
+
+    public class Sheep extends Animal{
+
+    }
+
+    public class Bull extends Animal{
+
+    }
+
+    public class Goat extends Animal{
+
+    }
+
+}
