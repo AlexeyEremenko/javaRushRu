fd21fe27b62b638d18f1f73095e7715eee2162d3
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-14 10:14:32 +0300

    arose from supper
    arose from supper, and laid aside his outer garments. He took a towel, and wrapped a towel around his waist (John 13:4)

---
+++ task11/task1120/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task11.task1120;
+
+//arose from supper, and laid aside his outer garments. He took a towel, and wrapped a towel around his waist (John 13:4)
+
+/* 
+Пятая правильная «цепочка наследования»
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class House extends Assets {
+
+    }
+
+    public class Cat extends Animal {
+
+    }
+
+    public class Car extends Assets {
+
+    }
+
+    public class Dog extends Animal {
+
+    }
+
+    public class Animal {
+
+    }
+
+    public class Assets {
+
+    }
+}
