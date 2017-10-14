0bb1c341d5f90b1ade1046ead8ad1d6161c63c55
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-14 09:52:17 +0300

    I came not to judge the world, but to save world
    And if any man hear my words, and believe not, I judge him not: for I came not to judge the world, but to save the world. (John 12:47)

---
+++ task11/task1113/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task11.task1113;
+
+//And if any man hear my words, and believe not, I judge him not: for I came not to judge the world, but to save the world. (John 12:47)
+
+/* 
+Играем в Дарвина
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Fish {
+
+    }
+
+    public class Animal extends Fish {
+
+    }
+
+    public class Ape extends Animal {
+
+    }
+
+    public class Human extends Ape {
+
+    }
+
+}
