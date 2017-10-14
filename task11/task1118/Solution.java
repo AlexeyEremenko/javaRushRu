9bc5c45ae4f1c1e445f76404ff0834ac743193f2
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-14 10:09:16 +0300

    During supper
    During supper, the devil having already put into the heart of Judas Iscariot, Simon's son, to betray him (John 13:2)

---
+++ task11/task1118/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task11.task1118;
+
+//During supper, the devil having already put into the heart of Judas Iscariot, Simon's son, to betray him (John 13:2)
+
+/* 
+Городское хозяйство
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Car {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+}
