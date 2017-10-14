08eaa157fa80209f37c6ac48eb03ff076d4a4cbc
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-14 10:00:12 +0300

    his commandment is eternal life
    I know that his commandment is eternal life. The things therefore which I speak, even as the Father has said to me, so I speak. (John 12:50)

---
+++ task11/task1116/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task11.task1116;
+
+//I know that his commandment is eternal life. 
+//The things therefore which I speak, even as the Father has said to me, so I speak. (John 12:50)
+
+/* 
+Цепочка наследования
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+}
