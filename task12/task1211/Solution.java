d932cfebfebf680bb1e56a264b22b680ab7abcc6
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-14 16:50:15 +0300

    I know whom I have chosen
    I don't speak concerning all of you. I know whom I have chosen. But that the Scripture may be fulfilled, 'He who eats bread with me has lifted up his heel against me.' (John 13:18)

---
+++ task12/task1211/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task12.task1211;
+
+//I don't speak concerning all of you. I know whom I have chosen. But that the Scripture may be fulfilled, 
+//'He who eats bread with me has lifted up his heel against me.' (John 13:18)
+
+/* 
+Абстрактный класс Pet
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Pet {
+        public String getName() {
+            return "Я - котенок";
+        }
+    }
+
+}
