dee994ce6bb29e580020ce74cc50dc3114678873
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-14 15:35:54 +0300

    you also ought to wash one another's feet
    If I then, the Lord and the Teacher, have washed your feet, you also ought to wash one another's feet. (John 13:14)

---
+++ task12/task1207/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1207;
+
+//If I then, the Lord and the Teacher, have washed your feet, you also ought to wash one another's feet. (John 13:14)
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    int a = 0;
+    print(a);
+    print((Integer)a);
+    }
+
+    public static void print(int a) {}//Напишите тут ваши методы
+    public static void print(Integer a) {}
+}
