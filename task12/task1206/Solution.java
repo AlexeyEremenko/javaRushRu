53c4e6eb6f978cb8647b800c0b2b50cb08342fa5
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-14 15:16:29 +0300

    You call me Teacher,Lord. You say correct: so I am
    You call me, 'Teacher' and 'Lord.' You say so correctly, for so I am. (John 13:13)

---
+++ task12/task1206/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task12.task1206;
+
+//You call me, 'Teacher' and 'Lord.' You say so correctly, for so I am. (John 13:13)
+
+/* 
+Выполняем перегрузку!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int a) {}//Напишите тут ваши методы
+    public static void print(String a) {}
+}
