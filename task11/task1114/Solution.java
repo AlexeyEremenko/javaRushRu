171ed2706bed29bfb5d6c8fc8798425fae44274c
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-14 09:54:56 +0300

    last day
    He who rejects me, and doesn't receive my sayings, has one who judges him. The word that I spoke, the same will judge him in the last day. (John 12:48)

---
+++ task11/task1114/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task11.task1114;
+
+//He who rejects me, and doesn't receive my sayings, has one who judges him.
+//The word that I spoke, the same will judge him in the last day. (John 12:48)
+
+/* 
+Все мировые религии
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Judaism {
+
+    }
+
+    public class Christianity extends Judaism {
+
+    }
+
+    public class Islam extends Christianity {
+
+    }
+
+}
