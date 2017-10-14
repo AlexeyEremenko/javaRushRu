adfc1a810afc93f297e6fb6974382de600f7cc9a
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-14 10:11:48 +0300

    Father had given all things into his hands
    Jesus, knowing that the Father had given all things into his hands, and that he came forth from God, and was going to God (John 13:3)

---
+++ task11/task1119/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task11.task1119;
+
+Jesus, knowing that the Father had given all things into his hands, and that he came forth from God, and was going to God (John 13:3)
+
+/* 
+Четвертая правильная «цепочка наследования»
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class House {
+
+    }
+
+    public class Cat {
+
+    }
+
+    public class Car {
+
+    }
+
+    public class Dog {
+
+    }
+}
