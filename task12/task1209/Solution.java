7c62f20c3bfd351198e4fe288622f1536c8fe04f
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-14 15:46:27 +0300

    The servant is not greater than his lord
    Verily, verily, I say unto you, The servant is not greater than his lord; neither he that is sent greater than he that sent him. (John 13:16)

---
+++ task12/task1209/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task12.task1209;
+
+//Verily, verily, I say unto you, The servant is not greater than his lord; 
+//neither he that is sent greater than he that sent him. (John 13:16)
+
+/* 
+Три метода и минимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int min(int x, int y) {
+        if (x>y) {return y;}else{return x;}
+    }//Напишите тут ваши методы
+    public static long min(long x, long y) {
+        if (x>y) {return y;}else{return x;}
+    }
+    public static double min(double x, double y) {
+        if (x>y) {return y;}else{return x;}
+    }
+}
