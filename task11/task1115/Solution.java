e4ef2c4e8704e9d8bbb1d6392c80ffcb97dddc38
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-14 09:57:45 +0300

    Father who sent me, he gave me a commandment
    For I spoke not from myself, but the Father who sent me, he gave me a commandment, what I should say, and what I should speak. (John 12:49)

---
+++ task11/task1115/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task11.task1115;
+
+//For I spoke not from myself, but the Father who sent me, he gave me a commandment,
+//what I should say, and what I should speak. (John 12:49)
+
+/* 
+От школьника до квалифицированного раба
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Schoolboy {
+
+    }
+
+    public class Student extends Schoolboy {
+
+    }
+
+    public class Worker extends Student {
+
+    }
+
+    public class Slave extends Worker {
+
+    }
+
+}
