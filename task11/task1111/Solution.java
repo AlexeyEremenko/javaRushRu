c8785b17baa9a030a0304045962e9c1a3ea3a0cb
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-14 06:55:18 +0300

    who sent me
    Jesus cried out and said, "Whoever believes in me, believes not in me, but in him who sent me. (John 12:44)

---
+++ task11/task1111/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task11.task1111;
+
+//Jesus cried out and said, "Whoever believes in me, believes not in me, but in him who sent me. (John 12:44)
+
+/* 
+«Из ребра его»
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    //Адам
+    public class Adam {
+
+    }
+
+    //Ева
+    public class Eve extends Adam {
+
+    }
+}
