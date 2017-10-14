5e59a8d758290dbfb39e08160489d9a9081456b2
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-14 10:20:34 +0300

    Lord, do you wash my feet?
    Then he came to Simon Peter. He said to him, "Lord, do you wash my feet?" (John 13:6)

---
+++ task11/task1122/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task11.task1122;
+
+//Then he came to Simon Peter. He said to him, "Lord, do you wash my feet?" (John 13:6)
+
+/* 
+Помощь шахматной школе
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+    
+    public class ChessFigure {}
+    
+    public class King extends ChessFigure {
+    }
+
+    public class Queen extends ChessFigure {
+    }
+
+    public class Rook extends ChessFigure {
+    }
+
+    public class Knight extends ChessFigure {
+    }
+
+    public class Bishop extends ChessFigure {
+    }
+
+    public class Pawn extends ChessFigure {
+    }
+}
