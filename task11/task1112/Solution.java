a7201220825ab7e9cd064bb4a78133ab88dc5537
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-14 09:44:43 +0300

    I have come as a light into the world
    I have come as a light into the world, that whoever believes in me may not remain in the darkness. (John 12:46)

---
+++ task11/task1112/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task11.task1112;
+
+//I have come as a light into the world, that whoever believes in me may not remain in the darkness. (John 12:46)
+
+/* 
+Заклятые друзья
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class AppleIPhone {
+
+    }
+
+    public class SamsungGalaxyS2 extends AppleIPhone {
+
+    }
+}
