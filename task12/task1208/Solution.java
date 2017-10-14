5d8ad66db6ad1026934f1553b7892a782d06a369
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-14 15:40:26 +0300

    I have given you an example
    For I have given you an example, that you also should do as I have done to you. (John 13:15)

---
+++ task12/task1208/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1208;
+
+//For I have given you an example, that you also should do as I have done to you. (John 13:15)
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int a) {}//Напишите тут ваши методы
+    public static void print(String a) {}
+    public static void print(double a) {}
+    public static void print(float a) {}
+    public static void print(Object a) {}
+}
