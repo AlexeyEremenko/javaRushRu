04ee0e0791cbc82ba0ed2247b9d0696ea1e0994b
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-14 10:17:23 +0300

    began to wash the disciples' feet
    Then he poured water into the basin, and began to wash the disciples' feet, and to wipe them with the towel that was wrapped around him. (John 13:5)

---
+++ task11/task1121/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task11.task1121;
+
+//Then he poured water into the basin, and began to wash the disciples' feet,
+//and to wipe them with the towel that was wrapped around him. (John 13:5)
+
+/* 
+Чужой код, незнакомый
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet  {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+
+    public class House {
+
+    }
+
+    public class Airplane {
+
+    }
+}
