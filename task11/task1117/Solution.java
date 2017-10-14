0efdc3858f964d869251c84be0541bae8dc18ac1
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-14 10:03:50 +0300

    he loved them to the end
    Now before the feast of the Passover, Jesus, knowing that his time had come that he would depart from this world to the Father, having loved his own who were in the world, he loved them to the end. (John 13:1)

---
+++ task11/task1117/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task11.task1117;
+
+//Now before the feast of the Passover, Jesus, knowing that his time had come that he would depart from this world to the Father, 
+//having loved his own who were in the world, he loved them to the end. (John 13:1)
+
+/* 
+Альтернативная цепочка наследования
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Carnivore extends Animal{
+
+    }
+
+    public class Cow extends Animal {
+
+    }
+
+    public class Dog extends Carnivore {
+
+    }
+
+    public class Pig extends Animal {
+
+    }
+
+    public class Animal {
+
+    }
+}
