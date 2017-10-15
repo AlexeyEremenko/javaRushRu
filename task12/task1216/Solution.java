4397f60e418378bd15ffe19b1c9a2590f2521e99
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-15 14:24:50 +0300

    whom Jesus loved
    Now there was leaning on Jesus' bosom one of his disciples, whom Jesus loved. (John 13;23)

---
+++ task12/task1216/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task12.task1216;
+
+//Now there was leaning on Jesus' bosom one of his disciples, whom Jesus loved. (John 13;23)
+
+/* 
+Летать охота
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface Fly {//add an interface here - добавь интерфейс тут
+        void toBelieve();
+        void toReceive();
+    }
+}
+
+
+/*
+Летать охота
+Напиши свой public интерфейс Fly(летать).
+Добавь в него два метода.
+
+
+Требования:
+1. Класс Solution должен содержать интерфейс Fly.
+2. Интерфейс Fly должен быть публичным.
+3. Интерфейс Fly должен содержать два метода.
+4. Интерфейс Fly не должен иметь родителя.
+*/
