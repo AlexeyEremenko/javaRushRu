a609aa46abea4258ea00e0619166fac7515373e0
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-15 14:36:19 +0300

    Лететь, бежать и плыть
    Simon Peter therefore beckoned to him, and said to him, "Tell us who it is of whom he speaks." (John 13:24)

---
+++ task12/task1217/Solution.java
@@ -0,0 +1,55 @@
+package com.javarush.task.task12.task1217;
+
+//Simon Peter therefore beckoned to him, and said to him, "Tell us who it is of whom he speaks." (John 13:24)
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+public interface Fly {//add interfaces here - добавь интерфейсы тут
+    void toFly();
+}
+
+public interface Swim {
+    void toSwim();
+}
+
+public interface Run {
+    void toRun();
+}
+}
+
+
+/*
+Лететь, бежать и плыть
+Напиши public интерфейсы Fly(летать), Run(бежать/ездить), Swim(плавать).
+Добавить в каждый интерфейс по одному методу.
+
+
+Требования:
+1. Класс Solution должен содержать интерфейс Fly.
+2. Класс Solution должен содержать интерфейс Run.
+3. Класс Solution должен содержать интерфейс Swim.
+4. Интерфейс Fly должен содержать один метод.
+5. Интерфейс Run должен содержать один метод.
+6. Интерфейс Swim должен содержать один метод.
+package com.javarush.task.task12.task1217;
+
+ 
+Лететь, бежать и плыть
+
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+//add interfaces here - добавь интерфейсы тут
+
+}
+*/
