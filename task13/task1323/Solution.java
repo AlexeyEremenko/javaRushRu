a565537bd6d145881d00f72004169f85fb102bb6
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-16 18:34:50 +0300

    God has power to make him stand
    Who are you who judge another's servant? To his own lord he stands or falls. Yes, he will be made to stand, for God has power to make him stand. (Romans 14:4)

---
+++ task13/task1323/Solution.java
@@ -0,0 +1,64 @@
+package com.javarush.task.task13.task1323;
+
+//Who are you who judge another's servant? To his own lord he stands or falls. 
+//Yes, he will be made to stand, for God has power to make him stand. (Romans 14:4)
+
+/* 
+Интерфейс Updatable в классе Screen
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    interface Updatable extends Selectable {
+        void refresh();
+    }
+
+    class Screen implements Updatable {
+        public void refresh() {}
+        public void onSelect() {}
+    }
+}
+
+
+
+/*
+Интерфейс Updatable в классе Screen
+Реализовать интерфейс Updatable в классе Screen.
+
+
+Требования:
+1. Интерфейс Updatable должен наследовать(extends) интерфейс Selectable.
+2. Класс Screen должен реализовывать(implements) интерфейс Updatable.
+3. В классе Screen должен быть реализован метод onSelect интерфейса Selectable.
+4. В классе Screen должен быть реализован метод refresh интерфейса Updatable.
+
+package com.javarush.task.task13.task1323;
+
+* 
+Интерфейс Updatable в классе Screen
+*
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    interface Updatable extends Selectable {
+        void refresh();
+    }
+
+    class Screen {
+
+    }
+}
+
+*/
