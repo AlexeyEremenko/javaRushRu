b4eb72b354021131c83a83a633e386726e5ffcfe
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-23 20:34:33 +0300

    according to Christ Jesus
    Now the God of patience and of encouragement grant you to be of the same mind one with another according to Christ Jesus (Romans 15:5)

---
+++ task16/task1618/Solution.java
@@ -0,0 +1,61 @@
+
+//Now the God of patience and of encouragement grant you to be of the same mind one with another according to Christ Jesus (Romans 15:5)
+
+
+package com.javarush.task.task16.task1618;
+
+/* 
+Снова interrupt
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        TestThread tt = new TestThread();//Add your code here - добавь код тут
+        tt.start();
+        tt.interrupt();
+    }
+
+    //Add your code below - добавь код ниже
+    public static class TestThread extends Thread {
+        public void run() {
+            
+        }
+    }
+}
+
+
+
+
+
+/*
+Снова interrupt
+Создай нить TestThread.
+В методе main создай экземпляр нити, запусти, а потом прерви ее используя метод interrupt().
+
+
+Требования:
+1. Класс TestThread должен быть унаследован от Thread.
+2. Класс TestThread должен иметь public void метод run.
+3. Метод main должен создавать объект типа TestThread.
+4. Метод main должен вызывать метод start у объекта типа TestThread.
+5. Метод main должен вызывать метод interrupt у объекта типа TestThread.
+
+package com.javarush.task.task16.task1618;
+
+* 
+Снова interrupt
+*
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        //Add your code here - добавь код тут
+    }
+
+    //Add your code below - добавь код ниже
+    public static class TestThread extends Thread {
+        public void run() {
+            
+        }
+    }
+}
+*/
