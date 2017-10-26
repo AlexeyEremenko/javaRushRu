6141b4c4ab2b1b9b31dbba0a90d2b9c23a0dba69
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-26 06:46:28 +0300

    love with which you loved me may be in them
    I made known to them your name, and will make it known; that the love with which you loved me may be in them, and I in them. (John 17:26)

---
+++ task18/task1811/Solution.java
@@ -0,0 +1,13 @@
+
+//I made known to them your name, and will make it known; that the love with which you loved me may be in them, and I in them. (John 17:26)
+         love with which you loved me may be in them
+
+
+
+
+
+
+/*
+
+
+*/
