9162b8c448610af51172bc047b58e109d735b07a
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-27 07:04:31 +0300

    entered in with Jesus into z court of zHigh priest
    Simon Peter followed Jesus, as did another disciple. Now that disciple was known to the high priest, and entered in with Jesus into the court of the high priest (John 18:15)

---
+++ task18/task1826/Solution.java
@@ -0,0 +1,16 @@
+
+//Simon Peter followed Jesus, as did another disciple. Now that disciple was known to the high priest,
+//and entered in with Jesus into the court of the high priest (John 18:15)
+
+Simon Peter followed Jesus, as did another disciple. Now that disciple was known to the high priest, and entered in with Jesus into the court of the high priest (John 18:15)
+
+entered in with Jesus into z court of zHigh priest
+
+
+
+
+/*
+
+
+
+*/
