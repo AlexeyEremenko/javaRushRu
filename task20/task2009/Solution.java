7b2bf23999117680e28c8d972ca69b12c2a38247
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-28 21:39:20 +0300

    So Pilate then took Jesus, and flogged him
    So Pilate then took Jesus, and flogged him (John 19:1)

---
+++ task20/task2009/Solution.java
@@ -0,0 +1,50 @@
+
+//So Pilate then took Jesus, and flogged him (John 19:1)
+
+package com.javarush.task.task20.task2009;
+
+import java.io.Serializable;
+
+/* 
+Как сериализовать static?
+*/
+public class Solution {
+    public static class ClassWithStatic implements Serializable {
+        public static String staticString = "it's test static string";
+        public int i;
+        public int j;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+Как сериализовать static?
+Сделай так, чтобы сериализация класса ClassWithStatic была возможной.
+
+
+Требования:
+1. Класс ClassWithStatic должен существовать внутри класса Solution.
+2. Класс ClassWithStatic должен быть статическим.
+3. Класс ClassWithStatic должен быть публичным.
+4. Класс ClassWithStatic должен поддерживать интерфейс Serializable.
+
+package com.javarush.task.task20.task2009;
+
+* 
+Как сериализовать static?
+*
+public class Solution {
+    public static class ClassWithStatic {
+        public static String staticString = "it's test static string";
+        public int i;
+        public int j;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+*/
