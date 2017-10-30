d829be8be80142b78fab1e8ee4899c5a57867aee
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-30 17:51:22 +0200

    first fruits
    Greet the assembly that is in their house. Greet Epaenetus, my beloved, who is the first fruits of Achaia to Christ. (Romans 16:5)

---
+++ Multithreading/task21/task2103/Solution.java
@@ -0,0 +1,44 @@
+
+//Greet the assembly that is in their house. Greet Epaenetus, my beloved, who is the first fruits of Achaia to Christ. (Romans 16:5)
+
+package com.javarush.task.task21.task2103;
+
+/* 
+Все гениальное - просто!
+*/
+public class Solution {
+    public static boolean calculate(boolean a, boolean b, boolean c, boolean d) {
+        return c;
+    }    
+
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+Все гениальное - просто!
+Упростить. Переменные не переименовывать, комментарии не оставлять.
+
+
+Требования:
+1. Метод calculate должен быть статическим.
+2. Метод calculate должен возвращать значение типа boolean.
+3. Метод calculate должен принимать четыре параметра типа boolean.
+4. Метод calculate должен быть максимально упрощен(поведение должно остаться прежним).
+
+package com.javarush.task.task21.task2103;
+
+* 
+Все гениальное - просто!
+*
+public class Solution {
+    public static boolean calculate(boolean a, boolean b, boolean c, boolean d) {
+        return (a && b && c && !d) || (!a && c) || (!b && c) || (c && d);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+*/
