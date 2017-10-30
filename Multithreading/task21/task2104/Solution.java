83ab3d7a2db47990e8a1502fae3fb4c14d9589bc
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-30 19:26:25 +0200

    in Christ before me
    Greet Andronicus and Junia, my relatives and my fellow prisoners, who are notable among the apostles, who also were in Christ before me (John 16:7)

---
+++ Multithreading/task21/task2104/Solution.java
@@ -0,0 +1,148 @@
+
+//Greet Andronicus and Junia, my relatives and my fellow prisoners, who are notable among the apostles, who also were in Christ before me (John 16:7)
+
+package com.javarush.task.task21.task2104;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Equals and HashCode
+*/
+public class Solution {
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+     @Override
+    public boolean equals(Object o)
+    {
+        if (o == null)
+            return false;
+
+        if (!(o instanceof Solution)){
+            return false;
+        }
+
+        if (this == o) return true;
+
+        Solution solution = (Solution) o;
+
+        if (first != null ? !first.equals(solution.first) : solution.first != null) return false;
+        if (last != null ? !last.equals(solution.last) : solution.last != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (last != null ? last.hashCode() : 0);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Donald", "Duck"));
+        System.out.println(s.contains(new Solution("Donald", "Duck")));
+    }
+}package com.javarush.task.task21.task2104;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Equals and HashCode
+*/
+public class Solution {
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+     @Override
+    public boolean equals(Object o)
+    {
+        if (o == null)
+            return false;
+
+        if (!(o instanceof Solution)){
+            return false;
+        }
+
+        if (this == o) return true;
+
+        Solution solution = (Solution) o;
+
+        if (first != null ? !first.equals(solution.first) : solution.first != null) return false;
+        if (last != null ? !last.equals(solution.last) : solution.last != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (last != null ? last.hashCode() : 0);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Donald", "Duck"));
+        System.out.println(s.contains(new Solution("Donald", "Duck")));
+    }
+}
+
+/*
+Equals and HashCode
+В классе Solution исправить пару методов equals/hashCode в соответствии с правилами реализации этих методов(детали уточни у своего любимого поисковика).
+Обе строки first и last должны принимать участие в сравнении с помощью метода equals и вычислении hashcode.
+Метод main не участвует в тестировании.
+
+
+Требования:
+1. Хешкоды одинаковых объектов должны быть равны.
+2. Метод equals должен проверять равен ли переданный объект текущему(сравнение через ==).
+3. Метод equals должен проверять является ли переданный объект объектом класса Solution.
+4. Метод equals должен возвращать true в случае, если поля first и last равны у переданного объекта и текущего(не забудь что они могут быть равны null).
+5. Должно быть обеспечено корректное поведение HashSet с типом элементов Solution.
+
+package com.javarush.task.task21.task2104;
+
+import java.util.HashSet;
+import java.util.Set;
+
+* 
+Equals and HashCode
+*
+public class Solution {
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public boolean equals(Solution n) {
+        return n.first.equals(first) && n.last.equals(last);
+    }
+
+    public int hashCode() {
+        return 31 * first.hashCode() + last.hashCode();
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Donald", "Duck"));
+        System.out.println(s.contains(new Solution("Donald", "Duck")));
+    }
+}
+*/
