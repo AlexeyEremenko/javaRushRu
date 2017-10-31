f258d717b851cbdf4efecd37ccded8ec2cc36dca
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-31 16:58:16 +0200

    I desire to have you wise in that which is good
    For your obedience has become known to all. I rejoice therefore over you. But I desire to have you wise in that which is good, but innocent in that which is evil. (Romans 16:19)

---
+++ Multithreading/task21/task2114/Hippodrom.java
@@ -0,0 +1,26 @@
+
+//For your obedience has become known to all. I rejoice therefore over you. But I desire to have you wise in that which is good, but innocent in that which is evil. (Romans 16:19)
+
+ I desire to have you wise in that which is good
+
+
+
+
+
+/*
+Ипподром(2)
+Раз это ипподром, то на нем должны быть лошади.
+Значит наш ипподром должен хранить список всех его лошадей.
+Добавь поле horses типа List<Horse> в класс Hippodrome.
+А чтобы лошадей не украли — сделай это поле приватным.
+Добавь getter для этого поля.
+
+
+Требования:
+1. В классе Hippodrome должен быть создан список лошадей(horses).
+2. В классе Hippodrome должен быть создан метод getHorses возвращающий список horses.
+3. Метод getHorses должен быть объявлен с модификатором доступа public.
+4. Поле horses должно быть объявлено с модификатором доступа private.
+
+
+*/
