b69749c5186a0b5b4165a641e8832c7ab4484c3c
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-31 16:47:18 +0200

    serve our Lord, Jesus Christ
    For those who are such don't serve our Lord, Jesus Christ, but their own belly; and by their smooth and flattering speech, they deceive the hearts of the innocent. (Romans 16:18)

---
+++ Multithreading/task21/task2113/Hippodrome.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task21.task2113;
+
+//For those who are such don't serve our Lord, Jesus Christ, but their own belly; and by their smooth and flattering speech, they deceive the hearts of the innocent. (Romans 16:18)
+
+public class Hippodrome {
+    public static void main(String[] args) {}
+}
