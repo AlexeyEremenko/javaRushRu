22aa381b04f43454ba74d9464e71230e3e52a11e
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-11-02 22:49:23 +0200

    "It is finished."
    When Jesus therefore had received the vinegar, he said, "It is finished." He bowed his head, and gave up his spirit. (John 19:30)

---
+++ Multithreading/task22/task2208/Solution.java
@@ -0,0 +1,60 @@
+
+//When Jesus therefore had received the vinegar, he said, "It is finished." He bowed his head, and gave up his spirit. (John 19:30)
+
+package com.javarush.task.task22.task2208;
+
+import java.util.Map;
+import java.util.LinkedHashMap;
+
+/* 
+Формируем WHERE
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("name","Ivanov");
+        map.put("country","Ukraine");
+        map.put("city","Kiev");
+        map.put("age",null);
+        map.put("name1","Ivanov");
+        map.put("name2","Ivanov");
+
+        System.out.println(getQuery(map));
+    }
+
+    public static String getQuery(Map<String, String> params) {
+        StringBuilder result = new StringBuilder();
+        if (params == null || params.isEmpty())
+            return result.toString();
+
+        for (Map.Entry<String, String> pair : params.entrySet()) {
+            if (pair.getKey() == null || pair.getValue() == null)
+                continue;
+
+            result.append(pair.getKey()).append(" = '").append(pair.getValue()).append("' and ");
+        }
+
+        if (result.length() > 5)
+            result.delete(result.length() - 5, result.length());
+
+        return result.toString();
+    }
+}
+/*
+Формируем WHERE
+Сформируй часть запроса WHERE используя StringBuilder.
+Если значение null, то параметр не должен попадать в запрос.
+
+Пример:
+{"name", "Ivanov", "country", "Ukraine", "city", "Kiev", "age", null}
+
+Результат:
+"name = 'Ivanov' and country = 'Ukraine' and city = 'Kiev'"
+
+
+Требования:
+1. Метод getQuery должен принимать один параметр типа Map.
+2. Метод getQuery должен иметь тип возвращаемого значения String.
+3. Метод getQuery должен быть статическим.
+4. Метод getQuery должен возвращать строку сформированную по правилам описанным в условии задачи.
+*/
