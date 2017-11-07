888e728f666abaa5041b94d76166b3c2fde4e93a
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-11-07 23:16:30 +0200

    Tend my sheep
    He said to him again a second time, "Simon, son of Jonah, do you love me?" He said to him, "Yes, Lord; you know that I have affection for you." He said to him, "Tend my sheep." (John 21:16)

---
+++ Multithreading/task24/task3608/model/Model.java
@@ -0,0 +1,10 @@
+
+//He said to him again a second time, "Simon, son of Jonah, do you love me?" He said to him, "Yes, Lord; you know that I have affection for you." He said to him, "Tend my sheep." (John 21:16)
+
+package com.javarush.task.task36.task3608.model;
+
+
+public interface Model {
+    ModelData getModelData();
+    void loadUsers();
+}
