009ea00ceae588ed23296113ae32a0102f47fdfd
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-11-07 23:19:56 +0200

    Jesus said to him, "Feed my sheep"
    He said to him the third time, "Simon, son of Jonah, do you have affection for me?" Peter was grieved because he asked him the third time, "Do you have affection for me?" He said to him, "Lord, you know everything. You know that I have affection for you." Jesus said to him, "Feed my sheep. (John 21:17)

---
+++ Multithreading/task24/task3608/model/ModelData.java
@@ -0,0 +1,15 @@
+
+//He said to him the third time, "Simon, son of Jonah, do you have affection for me?" Peter was grieved because he asked him the third time, "Do you have affection for me?" He said to him, "Lord, you know everything. You know that I have affection for you." Jesus said to him, "Feed my sheep. (John 21:17)
+
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+import java.util.List;
+
+public class ModelData {
+    private List<User> users;
+    
+    public List<User> getUsers() {return users;}
+    
+    public void setUsers(List<User> users) {this.users = users;}
+}
