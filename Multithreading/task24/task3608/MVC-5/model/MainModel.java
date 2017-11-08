c79e54a28d5e753f01a66efadcf09dde387ae00e
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-11-08 22:34:23 +0200

    The light shines in the darkness
    The light shines in the darkness, and the darkness hasn't overcome it. (John 1:5)

---
+++ Multithreading/task24/task3608/MVC-5/model/MainModel.java
@@ -0,0 +1,65 @@
+
+//The light shines in the darkness, and the darkness hasn't overcome it. (John 1:5)
+
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.model.service.UserService;
+import com.javarush.task.task36.task3608.model.service.UserServiceImpl;
+
+import java.util.List;
+
+public class MainModel implements Model {
+    private ModelData modelData = new ModelData();
+    private UserService userService = new UserServiceImpl();
+    
+    public ModelData getModelData() {
+        return modelData;
+    }
+    
+    public void loadUsers() {
+        modelData.setUsers(userService.getUsersBetweenLevels(1, 100));
+    }
+    
+    public void loadDeletedUsers() {
+        //modelData.setDisplayDeletedUserList(true);
+        List<User> users = userService.getAllDeletedUsers();
+        modelData.setUsers(users);
+    }
+
+}
+
+/*
+MVC (5)
+В сервисе есть метод, который возвращает всех удаленных пользователей. Давай их отобразим.
+
+1. Распредели методы по классам MVC:
+
+public void fireEventShowDeletedUsers() {
+…onShowAllDeletedUsers();
+}
+
+public void onShowAllDeletedUsers() {
+…loadDeletedUsers();
+}
+
+public void loadDeletedUsers() {
+List<User> users = userService.getAllDeletedUsers();
+}
+
+Не забудь, что данные, полученные с сервера, необходимо положить в ModelData. А потом обновить view.
+Добавь это самостоятельно в нужные методы.
+
+2. Добавь в Solution.main вызов нового метода, который ты поместил в view.
+
+3. Добавь в интерфейс Model метод, который ты поместил в Модель, реализуй его в FakeModel: выброси UnsupportedOperationException.
+
+
+Требования:
+1. Необходимо определить правильное расположение метода fireEventShowDeletedUsers() и реализовать этот метод.
+2. Необходимо определить правильное расположение метода onShowAllDeletedUsers() и реализовать этот метод.
+3. Необходимо определить правильное расположение метода loadDeletedUsers() и реализовать этот метод.
+4. В методе main класса Solution необходимо вызвать метод, который ты ранее реализовал в классе UsersView.
+5. Интерфейс Model должен содержать объявление метода, который ты ранее реализовал в классе MainModel.
+6. В классе FakeModel в теле метода, помещенного в интерфейс Model, необходимо бросить UnsupportedOperationException.
+*/
