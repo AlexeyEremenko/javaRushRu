982d9de604b400a7a8e5007c01009f5bd8fe2d03
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-11-08 16:35:46 +0200

    God made foolish the wisdom of this world
    Where is the wise? where is the scribe? where is the disputer of this world? has not God made foolish the wisdom of this world? (1Cor 1:20)

---
+++ Multithreading/task25/task2501/Solution.java
@@ -0,0 +1,61 @@
+
+//Where is the wise? where is the scribe? where is the disputer of this world? has not God made foolish the wisdom of this world? (1Cor 1:20)
+
+package com.javarush.task.task25.task2501;
+
+/* 
+Новые возможности!
+*/
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(Alphabet.G.getLetterPosition());
+    }
+
+    public enum Alphabet {
+        A, B, C, D, E,
+        F, G, H, I, J,
+        K, L, M, N, O,
+        P, Q, R, S, T,
+        U, V, W, X, Y, Z;
+
+        int getLetterPosition() {
+            return ordinal()+1;
+        }
+    }
+}
+
+/*
+Новые возможности!
+Используя возможности enum-а реализуй метод getLetterPosition, который должен возвращать позицию буквы.
+Позиция буквы A — 1.
+
+
+Требования:
+1. Класс Solution должен содержать enum Alphabet.
+2. Alphabet должен содержать по порядку 26 букв английского алфавита.
+3. Метод getLetterPosition у класса Alphabet должен возвращать позицию буквы в алфавите.
+4. Метод getLetterPosition должен вызывать метод ordinal.
+
+package com.javarush.task.task25.task2501;
+
+* 
+Новые возможности!
+*
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(Alphabet.G.getLetterPosition());
+    }
+
+    public enum Alphabet {
+        A, B, C, D, E,
+        F, G, H, I, J,
+        K, L, M, N, O,
+        P, Q, R, S, T,
+        U, V, W, X, Y, Z;
+
+        int getLetterPosition() {
+            return -1;
+        }
+    }
+}
+*/
