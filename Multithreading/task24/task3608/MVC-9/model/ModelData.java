d2178b9bdcae924f0094ef544e044f7c5683ad48
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-11-10 10:44:51 +0200

    we have the mind of Christ.
    For who has known the mind of the Lord, that he may instruct him? But we have the mind of Christ. (1Cor 2:16)

---
+++ Multithreading/task24/task3608/MVC-9/model/ModelData.java
@@ -0,0 +1,26 @@
+
+//For who has known the mind of the Lord, that he may instruct him? But we have the mind of Christ. (1Cor 2:16)
+
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+import java.util.List;
+
+public class ModelData {
+    private List<User> users;
+    private User activeUser;
+    private boolean displayDeletedUserList;
+    
+    public List<User> getUsers() {return users;}
+    
+    public void setUsers(List<User> users) {this.users = users;}
+    
+    public User getActiveUser() {return activeUser;}
+    
+    public void setActiveUser(User activeUser) {this.activeUser = activeUser;}
+    
+    public boolean isDisplayDeletedUserList() {return displayDeletedUserList;}
+    
+    public void setDisplayDeletedUserList(boolean displayDeletedUserList) {
+        this.displayDeletedUserList = displayDeletedUserList;}
+}
