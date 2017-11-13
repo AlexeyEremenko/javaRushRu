ad03842eb04594c2d81b2ebca65b0244141ad790
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-11-13 17:00:06 +0200

    not think of men above that which is written
    And these things, brethren, I have in a figure transferred to myself and to Apollos for your sakes; that you might learn in us not to think of men above that which is written, that no one of you be puffed up for one against another. (1Cor 4:6)

---
+++ Multithreading/task26/task3008/ConsoleHelper.java
@@ -0,0 +1,34 @@
+
+//And these things, brethren, I have in a figure transferred to myself and to Apollos for your sakes; that you might learn in us not to think of men above that which is written, that no one of you be puffed up for one against another. (1Cor 4:6)
+
+package com.javarush.task.task30.task3008;
+
+
+public class ConsoleHelper {
+}
+
+/*
+Чат (1)
+Сегодня мы напишем чат. Набор программ с помощью которого можно будет
+обмениваться текстовыми сообщения. Набор будет состоять из одного сервера и
+нескольких клиентов, по одному для каждого участника чата.
+
+Начнем с сервера. Нам понадобятся такие классы:
+1) Server – основной класс сервера.
+2) MessageType – enum, который отвечает за тип сообщений пересылаемых между
+клиентом и сервером.
+3) Message – класс, отвечающий за пересылаемые сообщения.
+4) Connection – класс соединения между клиентом и сервером.
+5) ConsoleHelper – вспомогательный класс, для чтения или записи в консоль.
+
+Создай эти классы и добавь метод main в класс Server.
+
+
+Требования:
+1. Класс Server должен быть публичным и создан в отдельном файле.
+2. Перечисление MessageType должно быть публичным и создано в отдельном файле.
+3. Класс Message должен быть публичным и создан в отдельном файле.
+4. Класс Connection должен быть публичным и создан в отдельном файле.
+5. Класс ConsoleHelper должен быть публичным и создан в отдельном файле.
+6. В классе Server должен быть создан метод public static void main(String[] args).
+*/
