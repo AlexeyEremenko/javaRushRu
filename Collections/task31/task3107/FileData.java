b9ed3e76c0ae863a846ae564a5a412a8718b3986
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-12-28 17:46:09 +0200

    there was no more sea
    And I saw a new heaven and a new earth: for the first heaven and the first earth were passed away; and there was no more sea. (Revelation 21:1)

---
+++ Collections/task31/task3107/FileData.java
@@ -0,0 +1,30 @@
+
+//And I saw a new heaven and a new earth: for the first heaven and the first earth were passed away; and there was no more sea. (Revelation 21:1)
+
+package com.javarush.task.task31.task3107;
+
+public interface FileData {
+    boolean isHidden();
+
+    boolean isExecutable();
+
+    boolean isDirectory();
+
+    boolean isWritable();
+}
+
+/*
+Null Object Pattern
+Почитай на вики про паттерн "Null Object".
+Используй Files, чтобы в конструкторе класса Solution правильно инициализировать поле fileData объектом ConcreteFileData.
+Если возникли какие-то проблемы со чтением файла по пути pathToFile, то инициализируй поле объектом NullFileData.
+
+
+Требования:
+1. Конструктор Solution должен инициализировать поле fileData объектом ConcreteFileData.
+2. Если в конструкторе Solution возникла ошибка, нужно инициализировать поле fileData объектом NullFileData.
+3. Конструктор Solution должен корректно устанавливать значение поля hidden у объекта ConcreteFileData.
+4. Конструктор Solution должен корректно устанавливать значение поля executable у объекта ConcreteFileData.
+5. Конструктор Solution должен корректно устанавливать значение поля directory у объекта ConcreteFileData.
+6. Конструктор Solution должен корректно устанавливать значение поля writable у объекта ConcreteFileData.
+*/
