e01f5839b924a89d67a1e7e65c5f416b94b08527
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-12-29 18:34:41 +0200

    Father himself loveth u, because ye have loved me
    For the Father himself loveth you, because ye have loved me, and have believed that I came out from God. (John 16:27)

---
+++ Collections/task32/task3205/SomeInterfaceWithMethodsImpl.java
@@ -0,0 +1,58 @@
+
+//For the Father himself loveth you, because ye have loved me, and have believed that I came out from God. (John 16:27)
+
+package com.javarush.task.task32.task3205;
+
+public class SomeInterfaceWithMethodsImpl implements SomeInterfaceWithMethods {
+    public void voidMethodWithoutArgs() {
+        System.out.println("inside voidMethodWithoutArgs");
+    }
+
+    public String stringMethodWithoutArgs() {
+        System.out.println("inside stringMethodWithoutArgs");
+        return null;
+    }
+
+    public void voidMethodWithIntArg(int i) {
+        System.out.println("inside voidMethodWithIntArg");
+        voidMethodWithoutArgs();
+    }
+}
+
+/*
+Создание прокси-объекта
+
+1) В отдельном файле создай публичный класс CustomInvocationHandler, который будет хэндлером при создании прокси-объекта.
+
+2) CustomInvocationHandler должен поддерживать интерфейс InvocationHandler.
+
+3) CustomInvocationHandler должен иметь один публичный конструктор с одним аргументом типа SomeInterfaceWithMethods.
+
+4) Перед вызовом любого метода у оригинального объекта должна выводиться фраза [methodName in].
+
+5) После вызова любого метода у оригинального объекта должна выводиться фраза [methodName out].
+
+6) Реализуй логику метода getProxy, который должен создавать прокси (Proxy.newProxyInstance(...)).
+
+См. пример вывода в методе main.
+
+Метод main не участвует в тестировании.
+
+
+
+
+
+Требования:
+
+1. Класс CustomInvocationHandler должен существовать.
+
+2. Класс CustomInvocationHandler должен поддерживать интерфейс InvocationHandler.
+
+3. Класс CustomInvocationHandler должен иметь один публичный конструктор с одним аргументом типа SomeInterfaceWithMethods.
+
+4. Перед вызовом любого метода у оригинального объекта должна выводиться фраза [methodName in].
+
+5. После вызова любого метода у оригинального объекта должна выводиться фраза [methodName out].
+
+6. Метод getProxy должен создавать прокси для интерфейса SomeInterfaceWithMethods.
+*/
