1a3d0dfb1e3bf9e04002c545b5ef2bca47e6935e
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-12-30 15:56:45 +0200

    on the north three gates; on the south three gates
    On the east three gates; on the north three gates; on the south three gates; and on the west three gates. (Revelation 21:13)

---
+++ Collections/task32/task2213/Tetris3/Tetris.java
@@ -0,0 +1,47 @@
+
+//On the east three gates; on the north three gates; on the south three gates; and on the west three gates. (Revelation 21:13)
+
+package com.javarush.task.task22.task2213;
+
+public class Tetris {
+
+    private Field field;
+    private Figure figure;
+    public static void main(String[] args) {}
+
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+}
+
+/*
+Тетрис(3)
+
+Поле есть, фигура есть, а как их получить?
+
+
+
+Добавь геттеры для полей field и figure.
+
+
+
+
+
+Требования:
+
+1. В классе Tetris должен быть создан метод getField без параметров.
+
+2. В классе Tetris должен быть создан метод getFigure без параметров.
+
+3. Метод getField должен быть объявлен с самым широким типом доступа.
+
+4. Метод getFigure должен быть объявлен с самым широким типом доступа.
+
+5. Метод getField должен возвращать значение поля field.
+
+6. Метод getFigure должен возвращать значение поля figure.
+*/
