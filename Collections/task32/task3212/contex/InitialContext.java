3d838ab1d2f5899e45acc26e76c4d76dc8a6d90b
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-12-30 11:08:53 +0200

    and idolaters, and all liars
    But the fearful, and unbelieving, and the abominable, and murderers, and whoremongers, and sorcerers, and idolaters, and all liars, shall have their part in the lake which burneth with fire and brimstone: which is the second death. (Revelation 21:8)

---
+++ Collections/task32/task3212/contex/InitialContext.java
@@ -0,0 +1,62 @@
+
+//But the fearful, and unbelieving, and the abominable, and murderers, and whoremongers, and sorcerers, 
+//and idolaters, and all liars, shall have their part in the lake which burneth with fire and brimstone: which is the second death. (Revelation 21:8)
+
+package com.javarush.task.task32.task3212.contex;
+
+import com.javarush.task.task32.task3212.service.impl.EJBServiceImpl;
+import com.javarush.task.task32.task3212.service.impl.JMSServiceImpl;
+
+public class InitialContext {
+    public Object lookup(String jndiName) {
+
+        if (jndiName.equalsIgnoreCase("EJBService")) {
+            System.out.println("Looking up and creating a new EJBService object");
+            return new EJBServiceImpl();
+        } else if (jndiName.equalsIgnoreCase("JMSService")) {
+            System.out.println("Looking up and creating a new JMSService object");
+            return new JMSServiceImpl();
+        }
+        return null;
+    }
+}
+
+/*
+Service Locator
+
+Прочитать о паттерне Service locator.
+
+
+
+Реализуй логику метода getService(String jndiName) в ServiceLocator.
+
+В нем будет реализована работа с контекстом и кэшем.
+
+
+
+1) Верни из кэша нужный сервис.
+
+2) Если в кэше нет нужного сервиса то:
+
+2.1) Создай контекст.
+
+2.2) Возьми у контекста нужный сервис.
+
+2.3) Добавь сервис в кеш и верни его.
+
+
+
+
+
+Требования:
+
+1. Класс ServiceLocator должен содержать метод Service getService(String jndiName).
+
+2. Если нужный сервис находится в кэше, метод getService(String jndiName) должен возвращать сервис из кэша.
+
+3. Если нужный сервис НЕ находится в кэше, метод getService(String jndiName) должен создавать контекст.
+
+4. Если нужный сервис НЕ находится в кэше, метод getService(String jndiName) должен искать нужный сервис в контексте.
+
+5. Если нужный сервис НЕ находится в кэше, метод getService(String jndiName) должен добавлять в кэш сервис, найденный в контексте и возвращать его.
+*/
