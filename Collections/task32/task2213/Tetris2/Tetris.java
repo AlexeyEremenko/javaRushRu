82722af203469088120548c60f0065c667a8cebf
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-12-30 15:52:43 +0200

    had a wall great and high, and had twelve gates
    And had a wall great and high, and had twelve gates, and at the gates twelve angels, and names written thereon, which are the names of the twelve tribes of the children of Israel (Revelation 21:12)

---
+++ Collections/task32/task2213/Tetris2/Tetris.java
@@ -0,0 +1,33 @@
+
+//And had a wall great and high, and had twelve gates, and at the gates twelve angels, and names written thereon,
+//which are the names of the twelve tribes of the children of Israel (Revelation 21:12)
+
+package com.javarush.task.task22.task2213;
+
+public class Tetris {
+    private Field field;
+    private Figure figure;
+    public static void main(String[] args) {}
+}
+
+/*
+Тетрис(2)
+
+Отлично!
+
+Теперь добавь в класс Tetris два поля: field типа Field и figure типа Figure.
+
+
+
+
+
+Требования:
+
+1. Поле field типа Field должно быть создано в классе Tetris.
+
+2. Поле figure типа Figure должно быть создано в классе Tetris.
+
+3. Поле field должно быть объявлено с модификатором доступа private.
+
+4. Поле figure должно быть объявлено с модификатором доступа private.
+*/
