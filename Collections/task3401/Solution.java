2ff2742350e75eea22a6376c8220802e13197663
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2018-01-03 11:03:52 +0200

    [one] like the Son of man, clothed with a garment
    And in the midst of the seven candlesticks [one] like unto the Son of man, clothed with a garment down to the foot, and girt about the paps with a golden girdle (Revelation 1:13)

---
+++ Collections/task3401/Solution.java
@@ -0,0 +1,54 @@
+
+//And in the midst of the seven candlesticks [one] like unto the Son of man, clothed with a garment down to the foot,
+//and girt about the paps with a golden girdle (Revelation 1:13)
+
+package com.javarush.task.task34.task3401;
+
+/* 
+Числа Фибоначчи с помощью рекурсии
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.fibonacci(9));     //34
+        System.out.println(solution.fibonacci(5));     //5
+        System.out.println(solution.fibonacci(2));     //1
+        System.out.println(solution.fibonacci(1));     //1
+    }
+
+    public int fibonacci(int n) {
+        if (n == 0)
+            return 0;
+
+        if (n == 1)
+            return 1;
+
+        return fibonacci(n-1) + fibonacci(n-2);
+
+    }
+}
+
+/*
+Числа Фибоначчи с помощью рекурсии
+
+Почитай про числа Фибоначчи.
+
+Реализуй рекурсивную логику метода fibonacci, где n - это номер элемента в последовательности Фибоначчи.
+
+Не создавай в классе Solution дополнительные поля.
+
+
+
+
+
+Требования:
+
+1. В классе Solution не должны быть созданы дополнительные поля.
+
+2. Метод fibonacci должен принимать порядковый номер искомого числа последовательности Фибоначчи и возвращать его значение.
+
+3. Метод fibonacci не должен быть статическим.
+
+4. Метод fibonacci должен быть рекурсивным.
+*/
