8c2929b3736b62b2a62c35bba7cc861232efe7ad
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2018-01-04 10:55:37 +0200

    even to them that believe on his name
    But as many as received him, to them gave he power to become the sons of God, even to them that believe on his name (John 1:12)

---
+++ Multithreading/task27/task3110/Archiver7/exception/WrongZipFileException.java
@@ -0,0 +1,37 @@
+
+//But as many as received him, to them gave he power to become the sons of God, even to them that believe on his name (John 1:12)
+
+package com.javarush.task.task31.task3110.exception;
+
+public class WrongZipFileException extends Exception {
+}
+
+/*
+Archiver (7)
+
+Мы много всего сделали и можно немного передохнуть.
+
+Создадим, пакет exception и добавим в него два класса PathIsNotFoundException и WrongZipFileException.
+
+Исключение PathIsNotFoundException будем кидать, если не сможем найти путь, в который нужно распаковать архив, или путь к файлу, который хотим запаковать, или любой другой путь. Исключение WrongZipFileException будем кидать, если будет попытка сделать что-нибудь с архивом, который не
+
+существует.
+
+
+
+1. Создай пакет exception
+
+2. Добавь в него класс PathIsNotFoundException унаследованный от Exception
+
+3. Добавь класс WrongZipFileException, также унаследованный от Exception
+
+
+
+
+
+Требования:
+
+1. В пакете exception должен быть создан класс PathIsNotFoundException унаследованный от Exception.
+
+2. В пакете exception должен быть создан класс WrongZipFileException унаследованный от Exception.
+*/
