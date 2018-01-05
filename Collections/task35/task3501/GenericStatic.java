b93f1a0300eac096f0b16452ed19a55e8ae4cf30
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2018-01-05 22:32:09 +0200

    sun&air were darkened by reason of smoke of  pit
    And he opened the bottomless pit; and there arose a smoke out of the pit, as the smoke of a great furnace; and the sun and the air were darkened by reason of the smoke of the pit. (Revelation 9:2)

---
+++ Collections/task35/task3501/GenericStatic.java
@@ -0,0 +1,33 @@
+
+//And he opened the bottomless pit; and there arose a smoke out of the pit, as the smoke of a great furnace; and the sun and the air were darkened by reason of the smoke of the pit. (Revelation 9:2)
+
+package com.javarush.task.task35.task3501;
+
+public class GenericStatic {
+    public static<T> T someStaticMethod(T genericObject) {
+        System.out.println(genericObject);
+        return genericObject;
+    }
+}
+
+/*
+Вызов статического метода
+
+Измени статический метод в классе GenericStatic так, чтобы он стал дженериком.
+
+Пример вызова дан в методе main.
+
+
+
+
+
+Требования:
+
+1. Метод someStaticMethod в классе GenericStatic должен быть изменен в соответствии с условием задачи.
+
+2. Метод someStaticMethod должен быть статическим.
+
+3. Метод someStaticMethod должен быть публичным.
+
+4. Метод someStaticMethod должен выводить данные на экран.
+*/
