4189eaf3c1b959adfc846cd4079d197291ce30b1
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2018-01-05 22:56:30 +0200

    there came out of the smoke locusts upon the earth
    And there came out of the smoke locusts upon the earth: and unto them was given power, as the scorpions of the earth have power. (John 9:3)

---
+++ Collections/task35/task3503/InterfaceForGenerics.java
@@ -0,0 +1,33 @@
+
+//And there came out of the smoke locusts upon the earth: and unto them was given power, as the scorpions of the earth have power. (John 9:3)
+
+package com.javarush.task.task35.task3503;
+
+public interface InterfaceForGenerics {
+}
+
+/*
+Несколько суперклассов для дженерика
+
+Дан класс Solution, параметризированный T.
+
+Ограничьте параметр T.
+
+T должен быть наследником класса ClassForGenerics и одновременно реализовывать интерфейс InterfaceForGenerics.
+
+Менять можно только класс Solution.
+
+
+
+
+
+Требования:
+
+1. Класс Solution должен быть параметризирован типом который является наследником класса ClassForGenerics и одновременно реализовывать интерфейс InterfaceForGenerics.
+
+2. Интерфейс InterfaceForGenerics не должен расширять другие интерфейсы.
+
+3. Класс ClassForGenerics не должен поддерживать интерфейс InterfaceForGenerics.
+
+4. Класс Solution не должен быть наследником класса ClassForGenerics.
+*/
