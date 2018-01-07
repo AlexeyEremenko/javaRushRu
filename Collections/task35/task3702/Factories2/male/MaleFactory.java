64f94641f174009b7b829da608386d850774d1ff
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2018-01-07 19:02:43 +0200

    angel which had the trumpet
    Saying to the sixth angel which had the trumpet, Loose the four angels which are bound in the great river Euphrates. (Revelation 9:14)

---
+++ Collections/task35/task3702/Factories2/male/MaleFactory.java
@@ -0,0 +1,65 @@
+
+//Saying to the sixth angel which had the trumpet, Loose the four angels which are bound in the great river Euphrates. (Revelation 9:14)
+
+package com.javarush.task.task37.task3702.male;
+
+import com.javarush.task.task37.task3702.Human;
+
+public class MaleFactory { 
+
+    public Human getPerson(int age) {
+        if (age <= KidBoy.MAX_AGE)
+            return new KidBoy();
+        if (age <= TeenBoy.MAX_AGE)
+            return new TeenBoy();
+        return new Man();
+    }
+}
+
+/*
+Фабрики (2)
+
+1. Создай публичный класс MaleFactory в пакете male с публичным методом getPerson, который принимает один параметр int age (возраст) и возвращает нужный объект одного из классов, реализующих интерфейс Human.
+
+
+
+2. Логика метода getPerson: используя константу MAX_AGE определи, какой класс соответствует переданному возрасту.
+
+Например, age=4 соответствует мальчику (KidBoy), age=15 - подростку (TeenBoy), остальной возраст - взрослому мужчине.
+
+
+
+3. В методе main класса Solution создай фабрику и вызови у нее метод getPerson три раза с такими параметрами: 99, 4, 15.
+
+
+
+Выведи результат в консоль.
+
+Вывод должен быть следующий:
+
+Man{}
+
+KidBoy{}
+
+TeenBoy{}
+
+
+
+Молодец, Фабрика готова! Это паттерн Factory Method.
+
+
+
+
+
+Требования:
+
+1. Вывод на экран должен соответствовать условию задачи.
+
+2. В методе main класса Solution у MaleFactory должен быть вызван трижды метод getPerson с параметрами 99, 4, 15.
+
+3. Для возрастов больше 19 лет метод getPerson должен возвращать объект типа Man.
+
+4. Для возрастов больше 12 лет, но меньше 20, метод getPerson должен возвращать объект типа TeenBoy.
+
+5. Для возрастов меньше 13 лет, метод getPerson должен возвращать объект типа KidBoy.
+*/
