09ad5387a1fd7746a4647e195ed0b7ab415a0255
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2018-01-07 22:20:54 +0200

    heads of the horses were as the heads of lions
    And thus I saw the horses in the vision, and them that sat on them, having breastplates of fire, and of jacinth, and brimstone: and the heads of the horses were as the heads of lions; and out of their mouths issued fire and smoke and brimstone. (Revelation 9:17)

---
+++ Multithreading/task30/task3001/NumerationSystem.java
@@ -0,0 +1,37 @@
+
+//And thus I saw the horses in the vision, and them that sat on them, having breastplates of fire, and of jacinth,
+//and brimstone: and the heads of the horses were as the heads of lions; and out of their mouths issued fire and smoke and brimstone. (Revelation 9:17)
+
+package com.javarush.task.task30.task3001;
+
+public interface NumerationSystem {
+    int getNumerationSystemIntValue();
+}
+
+/*
+Конвертер систем счислений
+
+Реализуй логику метода convertNumberToOtherNumerationSystem, который должен переводить число number.getDigit(), из одной системы счисления (numerationSystem) в другую (expectedNumerationSystem).
+
+Брось NumberFormatException, если переданное число некорректно, например, число "120" с системой счисления 2.
+
+Валидация для - number.getDigit() - целое не отрицательное.
+
+Метод main не участвует в тестировании.
+
+
+
+
+
+Требования:
+
+1. Метод convertNumberToOtherNumerationSystem (Number, NumerationSystem), возвращающий тип Number, должен существовать.
+
+2. Должно бросаться исключение NumberFormatException, если переданное число некорректно в заданной системе счисления.
+
+3. Необходимо корректно реализовать метод convertNumberToOtherNumerationSystem - перевод числа в указанную систему счисления.
+
+4. Enum NumerationSystemType должен поддерживать интерфейс NumerationSystem.
+
+5. В enum-е NumerationSystemType должно присутствовать 11 значений оснований систем счисления. Основания: 2, 3, 4, 5, 6, 7, 8, 9, 10, 12 и 16.
+*/
