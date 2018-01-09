5c5df377ce3245077ad55b2ad42340da3e18a63d
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2018-01-09 23:28:47 +0200

    if anyman hurt them,fire proceedeth of their mouth
    And if any man will hurt them, fire proceedeth out of their mouth, and devoureth their enemies: and if any man will hurt them, he must in this manner be killed. (Revelation 11:5)

---
+++ Collections/task36/task3310/Shortener5/strategy/HashMapStorageStrategy.java
@@ -0,0 +1,72 @@
+
+//And if any man will hurt them, fire proceedeth out of their mouth, and devoureth their enemies: and if any man will hurt them, he must in this manner be killed. (Revelation 11:5)
+
+package com.javarush.task.task33.task3310.strategy;
+
+import java.util.HashMap;
+
+public class HashMapStorageStrategy implements StorageStrategy {
+
+    private HashMap<Long, String> data = new HashMap<>();;
+
+    @Override
+    public boolean containsKey(Long key) {
+        return data.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(String value) {
+        return data.containsValue(value);
+    }
+
+    @Override
+    public void put(Long key, String value) {
+        data.put(key, value);
+    }
+
+    @Override
+    public Long getKey(String value) {
+        for (Long key : data.keySet())
+            if (data.get(key).equals(value))
+                return key;
+
+        return null;
+    }
+
+    @Override
+    public String getValue(Long key) {
+        return data.get(key);
+    }
+}
+
+/*
+Shortener (5)
+
+Давай напишем наше первое хранилище (стратегию хранилища). Внутри оно будет содержать обычный HashMap. Все стратегии будем хранить в пакете strategy.
+
+5.1. Создай класс HashMapStorageStrategy, реализующий интерфейс StorageStrategy.
+
+5.2. Добавь в класс поле HashMap<Long, String> data. В нем будут храниться наши данные.
+
+5.3. Реализуй в классе все необходимые методы. Реализации методов должны использовать поле data. Дополнительные поля не создавать.
+
+
+
+
+
+Требования:
+
+1. Класс HashMapStorageStrategy должен поддерживать интерфейс StorageStrategy.
+
+2. В классе HashMapStorageStrategy должно быть создано поле data типа HashMap.
+
+3. В классе HashMapStorageStrategy должен быть корректно реализован метод containsKey.
+
+4. В классе HashMapStorageStrategy должен быть корректно реализован метод containsValue.
+
+5. В классе HashMapStorageStrategy должен быть корректно реализован метод put.
+
+6. В классе HashMapStorageStrategy должен быть корректно реализован метод getValue.
+
+7. В классе HashMapStorageStrategy должен быть корректно реализован метод getKey.
+*/
