1f52512ac74f6f2be613d58f45244a19580a2f65
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2018-01-10 15:41:36 +0200

    And they bare it
    And he saith unto them, Draw out now, and bear unto the governor of the feast. And they bare it. (John 2:8)

---
+++ Collections/task37/task3710/decorators/RedShapeDecorator.java
@@ -0,0 +1,59 @@
+
+//And he saith unto them, Draw out now, and bear unto the governor of the feast. And they bare it. (John 2:8)
+
+package com.javarush.task.task37.task3710.decorators;
+
+import com.javarush.task.task37.task3710.shapes.Shape;
+
+public class RedShapeDecorator extends ShapeDecorator {
+    private Shape shape;
+
+    public RedShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+        this.shape = decoratedShape;
+    }
+
+    private void setBorderColor(Shape shape) {
+        System.out.println(String.format("Setting border color for %s to red.", shape.getClass().getSimpleName()));
+    }
+
+    @Override
+    public void draw() {
+        setBorderColor(shape);
+        super.draw();
+    }
+}
+
+/*
+Decorator
+
+Создай класс RedShapeDecorator в пакете Decorators. Он должен расширять функциональность объектов типа Shape не меняя их структуру.
+
+
+
+Я уже создал абстрактный класс ShapeDecorator, поэтому:
+
+1) Класс RedShapeDecorator сделай наследником класса ShapeDecorator.
+
+2) Реализуй приватный метод setBorderColor с одним параметром типа Shape.
+
+Он должен выводить на экран фразу "Setting border color for XXX to red.", где XXX - имя конкретного декорируемого класса (можешь воспользоваться методами getClass().getSimpleName() вызванными на объекте полученном в качестве параметра).
+
+3) Переопредели метод draw, в нем сначала измени цвет отображаемого объекта с помощью метода setBorderColor, а потом нарисуй его.
+
+
+
+
+
+Требования:
+
+1. Класс RedShapeDecorator должен быть потомком класса ShapeDecorator.
+
+2. Метод setBorderColor должен выводить на экран фразу соответствующую условию задачи.
+
+3. Метод setBorderColor должен быть приватным.
+
+4. В методе draw класса RedShapeDecorator должен быть изменен цвет декорируемого объекта и вызван его метод draw.
+
+5. Публичный конструктор класса RedShapeDecorator должен принимать один параметр типа Shape.
+*/
