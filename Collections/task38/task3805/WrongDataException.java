34353b8d63015edbb02b95d827c55dbe3ef09143
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2018-01-11 13:36:03 +0200

    for he knew what was in man
    And needed not that any should testify of man: for he knew what was in man (John 2:25)

---
+++ Collections/task38/task3805/WrongDataException.java
@@ -0,0 +1,35 @@
+
+//And needed not that any should testify of man: for he knew what was in man (John 2:25)
+
+package com.javarush.task.task38.task3805;
+
+public class WrongDataException extends Exception {
+    public WrongDataException() {
+        super();
+    }
+    public WrongDataException(String message) {
+        super(message);
+    }
+}
+
+/*
+Улучшения в Java 7 (multiple exceptions)
+
+Перепиши реализации методов класса Solution.
+
+Используй нововведения (multiple exceptions), касающиеся обработки исключений, которые были добавлены в Java 7.
+
+
+
+
+
+Требования:
+
+1. В конструкторе класса Solution в одном catch блоке должны быть обработаны исключения WrongDataException и ConnectionException.
+
+2. В методе write в одном catch блоке должны быть обработаны исключения WrongDataException и ConnectionException.
+
+3. В методе read в одном catch блоке должны быть обработаны исключения WrongDataException и ConnectionException.
+
+4. В методе disconnect в одном catch блоке должны быть обработаны исключения WrongDataException и ConnectionException.
+*/
