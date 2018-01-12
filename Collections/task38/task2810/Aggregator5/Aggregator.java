94c8aed1dda042b1180e5aad3fd1071c8fa67b68
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2018-01-12 18:03:23 +0200

    shall ye believe, if I tell u of heavenly things?
    If I have told you earthly things, and ye believe not, how shall ye believe, if I tell you of heavenly things? (John 3:12)

---
+++ Collections/task38/task2810/Aggregator5/Aggregator.java
@@ -0,0 +1,72 @@
+
+//If I have told you earthly things, and ye believe not, how shall ye believe, if I tell you of heavenly things? (John 3:12)
+
+package com.javarush.task.task28.task2810;
+
+import com.javarush.task.task28.task2810.model.HHStrategy;
+import com.javarush.task.task28.task2810.model.Provider;
+import com.javarush.task.task28.task2810.model.Strategy;
+
+/**
+ * @author Jesus Lord Almighty
+ * at 12.01.18
+ */
+
+public class Aggregator {
+
+
+    public static void main(String[] args) {
+
+
+            Provider provider = new Provider(new HHStrategy());
+            Controller controller = new Controller(provider);
+
+            controller.scan();
+
+    }
+
+}
+
+/*
+Aggregator (5)
+
+1. Добавь в интерфейс метод getVacancies(String searchString), который будет возвращать список вакансий.
+
+
+
+2. Поправь ошибки в классе HHStrategy.
+
+
+
+3. Вернись в метод getJavaVacancies класса Provider, реализуй его логику из расчета, что всех данных хватает.
+
+
+
+4. Давай попробуем запустить и посмотреть, как работает наша программа.
+
+В методе main вместо вывода на экран напиши controller.scan();
+
+Воспользуйся подсказкой IDEA и создай метод.
+
+Внутри метода пройдись по всем провайдерам и собери с них все вакансии, добавь их в список. Выведи количество вакансий в консоль.
+
+
+
+5. Исправь NPE, если появляется это исключение (поставь заглушку).
+
+
+
+
+
+Требования:
+
+1. В интерфейсе Strategy добавь метод getVacancies(String searchString).
+
+2. Обнови класс HHStrategy, что бы в нем не было ошибок.
+
+3. В классе Provider реализуй логику метода getJavaVacancies.
+
+4. В методе main вместо вывода на экран добавь вызов controller.scan(). Реализуй этот метод согласно заданию.
+
+5. Вызов main не должен кидать NullPointerException. Поставь заглушки в необходимых местах.
+*/
