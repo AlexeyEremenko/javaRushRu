f1b094f0a4a6db5963e6872f2b77baf6942f5e67
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2018-01-12 11:07:17 +0200

    How can a man be born when he is old?
    Nicodemus saith unto him, How can a man be born when he is old? can he enter the second time into his mother's womb, and be born? (John 3:4)

---
+++ Collections/task38/task3809/IncorrectAccount.java
@@ -0,0 +1,35 @@
+
+//Nicodemus saith unto him, How can a man be born when he is old? can he enter the second time into his mother's womb, and be born? (John 3:4)
+
+package com.javarush.task.task38.task3809;
+
+public class IncorrectAccount {
+    @LongPositive
+    private String amountString;
+}
+
+/*
+Annotation + Reflection
+
+Разберись в коде и исправьте ошибку.
+
+Ориентируйся на ожидаемый вывод.
+
+Сделай минимально возможные изменения.
+
+
+
+
+
+Требования:
+
+1. Программа должна выводить данные на экран.
+
+2. Вывод на экран должен соответствовать ожидаемому.
+
+3. Аннотация(@interface) LongPositive должна быть отмечена двумя другими аннотациями.
+
+4. Аннотация(@interface) LongPositive должна применяться только к полям.
+
+5. Аннотация(@interface) LongPositive должна быть доступна во время выполнения программы.
+*/
