81ce9a4e808c07309588cc692df35dba1d0f6471
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2018-01-12 11:51:55 +0200

    Jesus answered, Verily, verily, I say unto thee
    Jesus answered, Verily, verily, I say unto thee, Except a man be born of water and of the Spirit, he cannot enter into the kingdom of God. (John 3:5)

---
+++ Collections/task38/task3810/Date.java
@@ -0,0 +1,33 @@
+
+//Jesus answered, Verily, verily, I say unto thee, Except a man be born of water and of the Spirit, he cannot enter into the kingdom of God. (John 3:5)
+
+package com.javarush.task.task38.task3810;
+
+public @interface Date {
+    int day(); int month(); int year(); //напиши свой код
+    int hour();int minute();int second();
+}
+
+/*
+Реализуй аннотации
+
+Проект должен компилироваться и выводить аннотацию класса Solution.
+
+Класс Solution и его аннотацию не менять.
+
+Аннотация Changelog должна быть видна во время выполнения.
+
+
+
+
+
+Требования:
+
+1. Вывод на экран должен соответствовать ожидаемому.
+
+2. На экран должна быть выведена аннотация @Changelog класса Solution.
+
+3. Аннотация @Changelog должна быть видна во время выполнения программы.
+
+4. В классе Solution должен быть реализован метод main с одним параметром типа String[].
+*/
